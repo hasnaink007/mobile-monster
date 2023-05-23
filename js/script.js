60e00c76afba833a855d6f0c794c2b53c12ccdd9
Umair Akram <contactumairakram@gmail.com>
2023-05-23 22:34:47 +0500

    dual loading

--- js/script.js
+++ js/script.js
@@ -1566,6 +1566,7 @@ function triggerDirectPage() {
     
     if(searchParams.get('cart') && searchParams.get('cart') == "true") {
 
+        loadReviewCart();
         setTimeout(function() {
 
             loadReviewCart();
