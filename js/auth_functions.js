185c5499d2512d1a87e618a3b1059b08e9a1cb32
Hasnain <36701274+hasnaink007@users.noreply.github.com>
2023-08-16 18:38:58 +0500

    Update auth_functions.js
    Change Domain

--- js/auth_functions.js
+++ js/auth_functions.js
@@ -3,10 +3,10 @@
 var isDev = new URLSearchParams(window.location.search).get('hks_dev') == 'true';
 
 
-var endpointUrl = 'https://mmbuild.shop/version-test';
+var endpointUrl = 'https://portal.mobilemonster.com.au/version-test';
 
 if(!isDev) {
-    endpointUrl = 'https://mmbuild.shop';
+    endpointUrl = 'https://portal.mobilemonster.com.au';
 }
 
 function setLocalDataValue(key, value) {
