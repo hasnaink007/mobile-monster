751bf16a8bf6169e8ec4edb310b714641ca993e8
Hasnain <36701274+hasnaink007@users.noreply.github.com>
2024-06-24 15:23:01 +0500

    Update jbhifi_jsonFormat.php
    Code modification

--- jbhifi_jsonFormat.php
+++ jbhifi_jsonFormat.php
@@ -1,8 +1,17 @@
 <?php
 
-  error_reporting(E_ALL);
+	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 	header('Content-Type: application/json');
-
+	
+	$postData = file_get_contents('php://input');
+	$data = json_decode($postData, true);
+	
+	if (json_last_error() !== JSON_ERROR_NONE) {
+	    echo json_encode(['error' => 'Invalid JSON data']);
+	    exit;
+	}
+	
+	echo json_encode(['received_data' => $data]);
 
 ?>
