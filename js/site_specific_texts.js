4a5d403fa8d76023a590d5ba62d60362bcd181fd
Faisal Saleem <hkstechlabs@gmail.com>
2025-04-18 12:41:13 +0500

    changes

--- js/site_specific_texts.js
+++ js/site_specific_texts.js
@@ -1,7 +1,7 @@
 // UK site
 if(window.location.host == 'mobilemonster.uk'){
     $('.australian-british').text('British') ;
-    $('.replace-to-$15').text('£15') ;
+    // $('.replace-to-$15').text('£15') ;
     $('.change-business-days').text('1 business day') ;
     $('.replace_273m_img').attr('src', 'https://cdn.prod.website-files.com/6295ec8d6adf7dabeda41909/6801682bfb3c561b0445a3a6_Group%201948755069.svg') ;
 }
