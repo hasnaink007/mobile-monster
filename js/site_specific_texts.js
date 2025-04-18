2023b45a74597125f3dea9f95d131a15550c0663
Ejaz Ahmad <ejazahmad4772@gmail.com>
2025-04-18 12:43:38 +0500

    site specific js

--- js/site_specific_texts.js
+++ js/site_specific_texts.js
@@ -14,7 +14,7 @@ if(window.location.host == 'mobilemonster.uk' || window.location.host.startsWith
     $('.replace_273m_img').attr('src', 'https://cdn.prod.website-files.com/6295ec8d6adf7dabeda41909/6801682bfb3c561b0445a3a6_Group%201948755069.svg') ;
 }
 
-if(!window.location.host == 'mobilemonster.uk'){
+if(window.location.host != 'mobilemonster.uk'){
     // hide/show uk related registration fields
     $('#signup_house_field').hide()
     $('#signup_city_field').hide()
