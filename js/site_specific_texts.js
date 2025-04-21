3ae56830d4c9d748da00d9a690feb7df5ec2340c
Faisal Saleem <hkstechlabs@gmail.com>
2025-04-21 11:59:46 +0500

    pull changes from main

--- js/site_specific_texts.js
+++ js/site_specific_texts.js
@@ -7,11 +7,13 @@ if(window.location.host == 'mobilemonster.uk' || window.location.host.startsWith
     $('#signup_state_field').hide()
     $('#signup_suburb_field').hide()
 
-
     $('.australian-british').text('British') ;
     $('.replace_max_amount').text('£15') ;
     $('.change-business-days').text('1 business day') ;
     $('.replace_monster_img').attr('src', 'https://cdn.prod.website-files.com/6295ec8d6adf7dabeda41909/6801682bfb3c561b0445a3a6_Group%201948755069.svg') ;
+    $('.change-uk-address').html(
+        'MobileMonster C/O BlankIT Ltd<br>Unit 43 Parkhall Biz Village<br>Parkhall Road<br>Stoke on Trent<br>ST3 5XA'
+    );
 }
 
 if(!window.location.host == 'mobilemonster.uk'){
