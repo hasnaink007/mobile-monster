74c5c7132a80b4850a5c8a19152663434f912849
Ejaz Ahmad <ejazahmad4772@gmail.com>
2025-04-22 14:49:23 +0500

    text change

--- js/site_specific_texts.js
+++ js/site_specific_texts.js
@@ -7,17 +7,23 @@ if(window.location.host == 'mobilemonster.uk' || window.location.host.startsWith
     $('#signup_state_field').hide()
     $('#signup_suburb_field').hide()
     
-    
-    $('.australian-british').text('British') ;
-    $('.replace_max_amount').text('£15') ;
-    $('.change-business-days').text('1 business day') ;
-    $('.replace_monster_img').attr('src', 'https://cdn.prod.website-files.com/6295ec8d6adf7dabeda41909/6801682bfb3c561b0445a3a6_Group%201948755069.svg').attr('srcset','') ;
-    $('.change-uk-address').html('MobileMonster C/O BlankIT Ltd<br>Unit 43 Parkhall Biz Village<br>Parkhall Road<br>Stoke on Trent<br>ST3 5XA');
-    $('.uk-search-text').html('Send your device<br>to our mailing address');
-
-    
     $('.not-to-show-for-uk').hide();
     $('.uk-hidden').hide()
+    
+    $('.replace_monster_img').attr('src', 'https://cdn.prod.website-files.com/6295ec8d6adf7dabeda41909/6801682bfb3c561b0445a3a6_Group%201948755069.svg').attr('srcset','') ;
+    
+    let ukTexts = {
+        '.australian-british': 'British',
+        '.replace_max_amount': '£15',
+        '.change-business-days': '1 business day',
+        '.change-uk-address': 'MobileMonster C/O BlankIT Ltd<br>Unit 43 Parkhall Biz Village<br>Parkhall Road<br>Stoke on Trent<br>ST3 5XA',
+        '.uk-search-text': 'Send your device<br>to our mailing address',
+    }
+
+    Object.keys(ukTexts).forEach(key => {
+        $(key).html(ukTexts[key])
+    })
+
     $('.visibility-hidden').css({"visibility": "hidden"})
 
 
