07ec9aee78d3be92d640e689cf10b91b943a3879
Ejaz Ahmad <ejazahmad4772@gmail.com>
2025-04-22 17:44:39 +0500

    apple brand text changes

--- js/site_specific_texts.js
+++ js/site_specific_texts.js
@@ -8,6 +8,7 @@ if(window.location.host == 'mobilemonster.uk' || window.location.host.startsWith
     $('#signup_suburb_field').hide()
     $('.not-to-show-for-uk').hide();
     $('.uk-hidden').hide()
+    $('.visibility-hidden').css({"visibility": "hidden"})
 
     if(window.location.pathname.startsWith('/drop-off-option')){
         window.location.href = window.location.href.replace('/drop-off-option', '/shipping');
@@ -27,7 +28,17 @@ if(window.location.host == 'mobilemonster.uk' || window.location.host.startsWith
         $(key).html(ukTexts[key])
     })
 
-    $('.visibility-hidden').css({"visibility": "hidden"})
+    $('.sell-iphone-brand-description').html($('.sell-iphone-brand-description').html()?.replaceAll("\n", '')?.replace( new RegExp(`<p>When you trade in your iPhone, we’ll give you a fair and professional(.*)Post satchel for mailed submissions.</p>`,'ig'), `
+        <p>When you trade in your iPhone, we’ll give you a fair and professional appraisal of its value. To do so, we ask you to rate its condition according to one of the following categories:</p>
+        <ul>
+            <li><b>Brand New</b> - A brand new, unactivated device that comes in its original sealed box including its accessories</li>
+            <li><b>Excellent</b> - This device is used but has been very well looked after, it basically looks as if it was brand new with no signs of any marks of wear on the device. It must be fully functional and unlocked.</li>
+            <li><b>Very Good</b> - This is also a used device but unlike the previous grade shows signs of use in the form of minor to moderate scratches to the screen or body. It must be fully functional and unlocked</li>
+            <li><b>Poor</b> - This device physically shows heavy signs of use in the form deep scratches to the screen or body. It might also have small cracks to the glass and minor issues such as buttons or speakers not in functional condition. The device however must not have any missing parts, must not be bent or liquid damaged. Device must still be unlocked.</li>
+        </ul>
+        <p>We hear it all the time: I want to sell my iPhone, but I don’t know what it’s worth. Well, our process works like this: you make your assessment as above, then send the phone to us, free of charge. We’ll confirm its condition, make you an offer based on one of the categories above, with better prices offered for better condition. Our offers are always 5% above our competitors and, if you don’t like what you hear, we’ll send it back for free.</p>
+    `))
+
 
 
 }
