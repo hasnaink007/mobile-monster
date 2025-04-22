7d99b21226c2cc28af452a86f75454235d9e0076
Ejaz Ahmad <ejazahmad4772@gmail.com>
2025-04-22 11:31:44 +0500

    uk-hidden class

--- js/site_specific_texts.js
+++ js/site_specific_texts.js
@@ -6,12 +6,15 @@ if(window.location.host == 'mobilemonster.uk' || window.location.host.startsWith
     $('#signup_city_field').show()
     $('#signup_state_field').hide()
     $('#signup_suburb_field').hide()
-
-
+    
+    
     $('.australian-british').text('British') ;
     $('.replace_max_amount').text('£15') ;
     $('.change-business-days').text('1 business day') ;
     $('.replace_monster_img').attr('src', 'https://cdn.prod.website-files.com/6295ec8d6adf7dabeda41909/6801682bfb3c561b0445a3a6_Group%201948755069.svg').attr('srcset','')
+    
+    
+    $('.uk-hidden').hide()
 }
 
 if(window.location.host != 'mobilemonster.uk'){
